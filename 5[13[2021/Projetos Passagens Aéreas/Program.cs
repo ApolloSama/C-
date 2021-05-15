692ffe7f2878325154a59468091bf2a1b57d9abb
Pedro <pedro.intropedi@gmail.com>
2021-05-14 21:49:37 -0300

    Conteúdo C#

---
+++ 5[13[2021/Projetos Passagens Aéreas/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Projetos_Passagens_Aéreas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senhaLogin;
+            string senhaValida = "123456";
+            bool dadosValidos = false;
+            bool repeticaoCadastro = false;
+            int contadorCadastro = 0;
+
+            string[] nome = new string[5];
+            string[] origem = new string[5];
+            string[] destino = new string[5];
+            string[] data = new string[5];
+
+            Console.WriteLine("Seja bem vindo, digite a sua senha para validar o login:");
+            do
+            {
+                Console.WriteLine("Digite a sua senha");
+                senhaLogin = Console.ReadLine();
+
+                if (senhaLogin == "123456")
+                {
+                    Console.WriteLine("Senha válida, prosseguindo...");
+                }
+                else
+                {
+                    Console.WriteLine("Senha incorreta, tente novamente");
+                }
+
+            } while (senhaLogin != senhaValida);
+
+            do
+            {
+                Console.WriteLine("Digite o que deseja fazer: 1 (Cadastrar Passagem), 2 (Listar Passagens), 0 (Sair)");
+                string escolha = (Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case "1":
+                        {
+                            do
+                            {
+                               Console.WriteLine("Digite o seu nome:");
+                                nome[contadorCadastro] = (Console.ReadLine());
+                                Console.WriteLine("Digite a sua origem:");
+                                origem[contadorCadastro] = (Console.ReadLine());
+                                Console.WriteLine("Digite o seu destino");
+                                destino[contadorCadastro] = (Console.ReadLine());
+                                Console.WriteLine("Digite a data do voo");
+                                data[contadorCadastro] = (Console.ReadLine());
+                                Console.WriteLine("Deseja criar mais um cadastro? S/N");
+
+                                contadorCadastro++; 
+                            } while (dadosValidos);
+                        }
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "0":
+                        repeticaoCadastro = true;
+                        break;
+
+                    default:
+                        break;
+
+                } 
+            }while (!repeticaoCadastro);
+
+        }
+    }
+}
---
+++ 5[14[2021/Ex. Sistema De Produtos/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Ex._Sistema_De_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"
+            Você tem a            ██████╗ ███████╗███╗   ███╗    ██╗   ██╗██╗███╗   ██╗██████╗  ██████╗ ██╗
+            capacidade de         ██╔══██╗██╔════╝████╗ ████║    ██║   ██║██║████╗  ██║██╔══██╗██╔═══██╗██║
+            cadastrar até 10      ██████╔╝█████╗  ██╔████╔██║    ██║   ██║██║██╔██╗ ██║██║  ██║██║   ██║██║
+            produtos neste        ██╔══██╗██╔══╝  ██║╚██╔╝██║    ╚██╗ ██╔╝██║██║╚██╗██║██║  ██║██║   ██║╚═╝
+            sistema.              ██████╔╝███████╗██║ ╚═╝ ██║     ╚████╔╝ ██║██║ ╚████║██████╔╝╚██████╔╝██╗     
+                                  ╚═════╝ ╚══════╝╚═╝     ╚═╝      ╚═══╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ ╚═╝     
+                                
+            ------------------------------------------------------------------------------------------------------------------------------------------------
+            ");
+
+            do
+            {
+                Console.WriteLine("1 (Cadastrar produtos) 2 (Listar os produtos) 3(Mostrar o menu)");
+                escolha = Console.ReadLine();
+
+                switch (escolha)
+                {
+                    case "1":
+                        {
+                            CadastrarProduto();
+                        }
+                        break;
+
+                    case "2":
+                        ListarProdutos();
+                        break;
+
+                    case "3":
+                        MostrarMenu();
+                        break;
+                    default:
+                        break;
+                }
+            } while (true);
+        }
+
+        static string[] nome = new string[10];
+        static float[] preco = new float[10];
+        static string escolhaPromo;
+        static bool promocao = false;
+        static string escolha;
+        static int c = 1;
+        static string maisProduto;
+
+
+        static void CadastrarProduto()
+        {
+            do
+            {
+                Console.WriteLine($"Digite o nome do seu {c}º produto:");
+                nome[c] = Console.ReadLine().ToLower();
+                Console.WriteLine($"Digite o valor do seu {c}º produto");
+                preco[c] = float.Parse(Console.ReadLine());
+                Console.WriteLine("O produto estará em promoção? (S/N)");
+                escolhaPromo = Console.ReadLine().ToLower();
+                if (escolhaPromo == "s")
+                {
+                    promocao = true;
+                }
+
+                Console.WriteLine("Deseja cadastrar mais um produto? (S/N)");
+                maisProduto = Console.ReadLine().ToLower();
+
+                c++;
+            } while (maisProduto == "s");
+        }
+
+
+        static void ListarProdutos()
+        {
+            Console.WriteLine($"Nome      Preço\n");
+            for (var i = 1; i < c; i++)
+            {
+                Console.WriteLine($"{nome[i]}   R${preco[i]}");
+                if (promocao == true)
+                {
+                    escolhaPromo = nome[c];
+                    Console.WriteLine($"O produto {nome[i]} está em promoção, aproveite já!!!");
+                }
+            }
+        }
+
+        static void MostrarMenu()
+        {
+            Console.WriteLine("Carregando...");
+        }
+    }
+}
+
